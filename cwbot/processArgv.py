92821420c0656392ecbb785edc6a03c523aa04ba
rlbond86 <rlbond86@96408d3d-42d1-492f-8470-de6ea9014457>
2013-03-30 19:44:25 +0000

    - /data and /log folders are created automatically now.
    - removed /data and /log folders from version control.
    
    git-svn-id: svn://svn.code.sf.net/p/cwbot/code/trunk@8 96408d3d-42d1-492f-8470-de6ea9014457

--- cwbot/processArgv.py
+++ cwbot/processArgv.py
@@ -1,7 +1,16 @@
 import os
 import logging
+import errno
 from cwbot.RunProperties import RunProperties
 from cwbot import logConfig
+
+def _createDir(name):
+    try:
+        os.makedirs(name)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+    
 
 def processArgv(argv):
     """ Process the command line arguments and return a RunProperties object.
@@ -11,11 +20,16 @@ def processArgv(argv):
     cwd = os.getcwd()
     try:
         os.chdir(argv[1])
-    except:
+    except (SystemExit, KeyboardInterrupt):
+        raise
+    except Exception:
         try:
             os.chdir(os.getenv("HOME"))
         except:
             pass
+        
+    _createDir("data")
+    _createDir("log")
     
     loginFile = 'login.ini'
     adminFile = 'admin.ini'
