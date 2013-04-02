cf9f3b03aa80787110fdca3a60619d862fee83a2
rlbond86 <rlbond86@96408d3d-42d1-492f-8470-de6ea9014457>
2013-04-02 17:23:57 +0000

    Fixed a crippling bug from r14 where the MessageManager would crash if it received a Kmail by accessing mod.identity instead of mod.id. I really need more unit tests...
    Bumped version to 0.7.3
    
    git-svn-id: svn://svn.code.sf.net/p/cwbot/code/trunk@20 96408d3d-42d1-492f-8470-de6ea9014457

--- cwbot/RunProperties.py
+++ cwbot/RunProperties.py
@@ -33,7 +33,7 @@ class RunProperties(object):
     """
 
 
-    version = "0.7.2"
+    version = "0.7.3"
     def __init__(self, debugMode, loginFile, adminFile, 
                  originalDir=os.getcwd(), altLogin=None):
         self.debug = debugMode
