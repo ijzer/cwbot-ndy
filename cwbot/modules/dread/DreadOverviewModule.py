39066cf2b31b4d42fb7334b77e42c82bd8943957
rlbond86 <rlbond86@96408d3d-42d1-492f-8470-de6ea9014457>
2013-08-21 03:48:19 +0000

    * Changes to DreadOverviewModule to use LRT to check for which monsters are more populous * Also fixed a variable declaration in DreadChoicesModule
    git-svn-id: svn://svn.code.sf.net/p/cwbot/code/branches/dread2@77 96408d3d-42d1-492f-8470-de6ea9014457

--- cwbot/modules/dread/DreadOverviewModule.py
+++ cwbot/modules/dread/DreadOverviewModule.py
@@ -2,7 +2,7 @@ from cwbot.modules.BaseDungeonModule import (BaseDungeonModule, eventFilter,
                                              eventDbMatch)
 from cwbot.util.textProcessing import stringToList
 from cwbot.common.exceptions import FatalError
-
+import math
 
 def dreadPercent(n):
     if n >= 1000:
@@ -18,23 +18,38 @@ class DreadOverviewModule(BaseDungeonModule):
     Options:
         update-percent: comma-separated list of percent values to 
                         update progress on zones (25, 50, 75, 90)
+        p_error: floating point probability of error between 0 and 1.
+                 setting this lower will reduce the probability of a "miscall"
+                 on which side is dominant in a Dreadsylvania area, at the
+                 cost of taking longer to actually decide which side is
+                 leading. 
     """
     requiredCapabilities = ['chat', 'dread']
     _name = "dread-overview"
 
-    _balance = {0: ['B', 'W'], 1: ['G', 'Z'], 2: ['S', 'V']}
+    _balanceLetters = {0: ['W', 'B'], 1: ['Z', 'G'], 2: ['V', 'S']}
+    _monsters = ["werewolf", "bugbear", "zombie", 
+                 "ghost", "vampire", "skeleton"]
+    _plurals = {'werewolf': "werewolves", 'bugbear': "bugbears",
+                'zombie': "zombies", 'ghost': "ghosts",
+                'vampire': "vampires", 'skeleton': "skeletons"}
+    _areas = ["The Woods", "The Village", "The Castle"]
     
     def __init__(self, manager, identity, config):
-        nones = None, None, None
         self._done = None
-        self._killed = None
-        self._woodsBal, self._villageBal, self._castleBal = nones
-        self._woodsLvl, self._villageLvl, self._castleLvl = nones
+        self._killed = None # kills by area
+        self._kills = None # kills by monster type
+        self._balance = None
+        self._banished = None
+        self._level = None
         self._drunk = None
         self._kisses = None
         self._lockedAreas = None
         self._locked = None
         self._notifyPercent = None
+        self._logLikelihoodRatio = None # for sequential likelihood ratio tests
+        self._likelihoodCalled = None
+        self._perror = None
         
         super(DreadOverviewModule, self).__init__(manager, identity, config)
 
@@ -42,6 +57,9 @@ class DreadOverviewModule(BaseDungeonModule):
     def initialize(self, state, initData):
         self._db = initData['event-db']
         self._lockedAreas = [r for r in self._db if r['zone'] == "(unlock)"]
+        self._logLikelihoodRatio = state['likelihood']
+        self._likelihoodCalled = state['decided']
+        self._kills = {}
         self._processLog(initData)
         
     
@@ -49,10 +67,16 @@ class DreadOverviewModule(BaseDungeonModule):
         try:
             self._notifyPercent = map(int, stringToList(
                                             config.setdefault('update-percent',
-                                                              "25,50,75,90")))
+                                                        "25,50,75,90,100")))
         except ValueError:
             raise FatalError("update-percent must be a list of integers")
-    
+        try:
+            self._perror = float(config.setdefault("p_error", "0.001"))
+            if not 0 < self._perror < 1:
+                raise ValueError()
+        except ValueError:
+            raise FatalError("p_error must be a float between 0 and 1")
+
 
     def _areaInfo(self, areaNum):
         d = {}
@@ -60,20 +84,14 @@ class DreadOverviewModule(BaseDungeonModule):
         d['accessible'] = self._drunk >= areaNum * 1000
         d['killed'] = self._killed[areaNum]
         d['done'] = self._done[areaNum]
-        d['balance'] = {0: self._woodsBal,
-                        1: self._villageBal,
-                        2: self._castleBal}[areaNum]
+        d['balance'] = self._balance[areaNum]
         if d['balance'] == 0:
             d['balanceWinning'] = ""
         else:
-            idx = 0 if d['balance'] < 0 else 1
-            d['balanceWinning'] = self._balance[areaNum][idx]
-        d['level'] = {0: self._woodsLvl, 
-                      1: self._villageLvl, 
-                      2: self._castleLvl}[areaNum]
-        d['fullname'] = {0: "The Woods", 
-                         1: "The Village", 
-                         2: "The Castle"}[areaNum]
+            idx = 1 if d['balance'] < 0 else 0
+            d['balanceWinning'] = self._balanceLetters[areaNum][idx]
+        d['level'] = self._level[areaNum]
+        d['fullname'] = self._areas[areaNum]
         d['name'] = d['fullname'][4:]
         d['locked'] = [a['subzone'] for a in self._locked
                        if a['category'] == d['fullname']]
@@ -95,34 +113,129 @@ class DreadOverviewModule(BaseDungeonModule):
         if d['level'] > 1:
             txt += "({})".format(d['level'])
         if d['killed'] >= 1000:
-            txt += " BOSS"
+            txt += " BOSS "
         else:
-            txt += " {}%".format(int(d['killed']/10))
+            txt += " {}% ".format(int(d['killed']/10))
+            
+        calledStr = "?"
+        n = self._likelihoodCalled[areaNum]
+        if n is not None:
+            calledStr = self._balanceLetters[areaNum][n]
+
+        banishA = self._banished[self._monsters[2*areaNum]]
+        banishB = self._banished[self._monsters[2*areaNum+1]]
         
-        # balance
-        if d['balance'] == 0:
-            txt += " ="
-        else:
-            txt += " " + d['balanceWinning'] + "+"
-            txt += "{}".format(abs(d['balance']))
+        letterA, letterB = tuple(self._balanceLetters[areaNum])
+        
+        txtA = {0: letterA + letterA,
+                1: "-" + letterA,
+                2: "--"}[banishA]
+        txtB = {0: letterB + letterB,
+                1: letterB + "-",
+                2: "--"}[banishB]
+        txt += "[" + txtA + calledStr + txtB + "]"
         return txt
+    
+    
+    def _doLRT(self, oldBanished, oldKills):
+        # determine which monster is more numerous using the
+        # sequential likelihood ratio test
+        # but ignore the kills if the banishments have just changed
+        for i in range(3):
+            printRatio = False
+            monsterA = self._monsters[2*i]
+            monsterB = self._monsters[2*i+1]
+            
+            banishA = self._banished[monsterA]
+            banishB = self._banished[monsterB]
+                
+            if (oldBanished is None
+                    or self._killed[i] > 1000
+                    or self._done[i]):
+                pass
+            elif (banishA == oldBanished[monsterA] 
+                  and banishB == oldBanished[monsterB]):
+                useTotal = (banishA == 0 and banishB == 0)
+                if useTotal:
+                    self._logLikelihoodRatio[i] = 0
+                    oldKills[monsterA] = 0
+                    oldKills[monsterB] = 0
+
+                newKillsA = self._kills[monsterA] - oldKills.get(monsterA, 0)
+                newKillsB = self._kills[monsterB] - oldKills.get(monsterB, 0)
+                
+                
+                # initial probability of monster A is 0.6 or 0.4
+                # BOTH probabilities below are the probability of an A 
+                # appearing; one of the two probabilities is correct
+                
+                probFavoringA = (3.0 - banishA) / (5.0 - banishA - banishB)
+                probFavoringB = (2.0 - banishA) / (5.0 - banishA - banishB)
+
+                # make sure the probability of killing the "wrong" monster
+                # is non-zero (somebody might be in a fight or something!)
+                
+                probFavoringA = min(max(0.05, probFavoringA), 0.95)
+                probFavoringB = min(max(0.05, probFavoringB), 0.95)
+                
+                # probabilities of these kills (newKillsA and newKillsB)
+                # under both assumptions
+                
+                logProbA = (newKillsA * math.log(probFavoringA)
+                            + newKillsB * math.log(1 - probFavoringA))
+                logProbB = (newKillsA * math.log(probFavoringB)
+                            + newKillsB * math.log(1 - probFavoringB))
+
+                oldRatio = self._logLikelihoodRatio[i]
+                self._logLikelihoodRatio[i] += logProbA
+                self._logLikelihoodRatio[i] -= logProbB
+                if oldRatio != self._logLikelihoodRatio[i]:
+                    self.debugLog("Area {}: {} kills [{}, {}], "
+                                  "ratio {} -> {}"
+                                  .format(i, 
+                                          "total" if useTotal else "new",
+                                          newKillsA, newKillsB,
+                                          oldRatio, 
+                                          self._logLikelihoodRatio[i]))
+                
+                oldCalled = self._likelihoodCalled[i]
+                eta = math.log((1 - self._perror) / self._perror)
+                if self._logLikelihoodRatio[i] > eta:
+                    self._likelihoodCalled[i] = 0
+                elif self._logLikelihoodRatio < -eta:
+                    self._likelihoodCalled[i] = 1
+                elif abs(self._logLikelihoodRatio) < 0.5 * eta:
+                    self._likelihoodCalled[i] = None
+                if (oldCalled != self._likelihoodCalled[i]
+                        and self._likelihoodCalled[i] is not None):
+                    printRatio = True
+                    self.log("Called area {} for monster {}"
+                             .format(i, self._likelihoodCalled[i]))
+            else:
+                self.debugLog("Different banishments, skipping LRT...")
+                if (self._likelihoodCalled[i] is None
+                        and banishA + banishB > 0):
+                    self.chat("I don't know the distribution of {} to {} yet."
+                              .format(monsterA, monsterB))
+                elif banishA + banishB > 0:
+                    printRatio = True
+            if printRatio:
+                a,b = (3,2) if self._likelihoodCalled[i] == 0 else (2,3)
+                self.chat("The distribution of {} to {} is "
+                          "currently {}:{}."
+                          .format(self._plurals[monsterA], 
+                                  self._plurals[monsterB], 
+                                  a - banishA, b - banishB))
+
 
 
     def _processLog(self, raidlog):
         events = raidlog['events']
-        self._done = {}
-        self._done[0] = any(eventDbMatch(events, 
-            {'category': "The Woods", 
-             'zone': "(combat)",
-             'subzone': "boss"}))
-        self._done[1] = any(eventDbMatch(events, 
-            {'category': "The Village", 
-             'zone': "(combat)",
-             'subzone': "boss"}))
-        self._done[2]  = any(eventDbMatch(events, 
-            {'category': "The Castle", 
-             'zone': "(combat)",
-             'subzone': "boss"}))
+        self._done = [any(eventDbMatch(events, 
+                                       {'category': self._areas[i], 
+                                        'zone': "(combat)",
+                                        'subzone': "boss"}))
+                      for i in range(3)]
         self._drunk = raidlog['dread']['drunkenness']
         
         newKilled = [raidlog['dread'].get('forest', 0),
@@ -137,17 +250,32 @@ class DreadOverviewModule(BaseDungeonModule):
                                                                 int(new/10)))
                         break
         self._killed = newKilled
+
+        oldKills = self._kills
+        oldBanished = self._banished
+        self._banished = {} 
+        self._kills = {}        
+        prefix = r'defeated\s+(?:hot|cold|spooky|stench|sleaze)\s+'
+        for monster in self._monsters:
+            self._kills[monster] = (
+                sum(e['turns'] for e in eventFilter(events, 
+                                                    prefix + monster)))
+            self._banished[monster] = (
+                sum(e['turns'] for e in eventFilter(events,
+                                                    "drove some " + monster)))
+            
+        # do likelihood ratio test to determine which monsters are more
+        # populous
+        self._doLRT(oldBanished, oldKills)
+                        
+        self._balance = dict((i, (self._kills[self._monsters[2*i]]
+                                 - self._kills[self._monsters[2*i+1]]))
+                         for i in range(3))
         
-        self._woodsBal = (  sum(e['turns'] for e in eventFilter(events, r'defeated\s+(?:hot|cold|spooky|stench|sleaze)\s+werewolf'))
-                          - sum(e['turns'] for e in eventFilter(events, r'defeated\s+(?:hot|cold|spooky|stench|sleaze)\s+bugbear')))
-        self._villageBal = (  sum(e['turns'] for e in eventFilter(events, r'defeated\s+(?:hot|cold|spooky|stench|sleaze)\s+zombie'))
-                            - sum(e['turns'] for e in eventFilter(events, r'defeated\s+(?:hot|cold|spooky|stench|sleaze)\s+ghost')))
-        self._castleBal = (  sum(e['turns'] for e in eventFilter(events, r'defeated\s+(?:hot|cold|spooky|stench|sleaze)\s+vampire'))
-                           - sum(e['turns'] for e in eventFilter(events, r'defeated\s+(?:hot|cold|spooky|stench|sleaze)\s+skeleton')))
-        
-        self._woodsLvl   = 1 + sum(e['turns'] for e in eventFilter(events, "made the forest less"))
-        self._villageLvl = 1 + sum(e['turns'] for e in eventFilter(events, "made the village less"))
-        self._castleLvl  = 1 + sum(e['turns'] for e in eventFilter(events, "made the castle less"))
+        self._level = [1 + sum(e['turns'] 
+                               for e in eventFilter(events, 
+                                                    "made the " + a + " less"))
+                       for a in ["forest", "village", "castle"]]
         
         self._kisses = raidlog['dread'].get('kisses', 0)
         
@@ -156,6 +284,18 @@ class DreadOverviewModule(BaseDungeonModule):
             if not any(eventDbMatch(events, area)):
                 self._locked.append(area)
         return True
+    
+    
+    @property
+    def state(self):
+        return {'likelihood': self._logLikelihoodRatio,
+                'decided': self._likelihoodCalled}
+    
+    
+    @property
+    def initialState(self):
+        return {'likelihood': [0, 0, 0],
+                'decided': [None, None, None]}
 
             
     def _processDungeon(self, txt, raidlog):
