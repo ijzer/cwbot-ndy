41834e72cb37dd4ca3bb22a652f23246971f982b
rlbond86 <rlbond86@96408d3d-42d1-492f-8470-de6ea9014457>
2013-08-29 14:32:43 +0000

    Fix a small off-by-one bug in ClanRankModule
    git-svn-id: svn://svn.code.sf.net/p/cwbot/code/branches/admissions@98 96408d3d-42d1-492f-8470-de6ea9014457

--- cwbot/modules/core/ClanRankModule.py
+++ cwbot/modules/core/ClanRankModule.py
@@ -513,7 +513,7 @@ class ClanRankModule(BaseModule):
                 if booted:
                     bootedMembers.append(uid_s)
             else:
-                nextCheckDays = _secsToDays(nextCheckTime - curTime)
+                nextCheckDays = 1 + _secsToDays(nextCheckTime - curTime)
                 self.debugLog("[{}/{}] Skipping boot check for {}; will "
                               "check in {} days."
                               .format(i, numRecords,
@@ -609,7 +609,7 @@ class ClanRankModule(BaseModule):
         # e.g., if I ban after 100 days and a user has been inactive for 40, 
         # check back in around 60 days.
         lastLogin = calendar.timegm(d1['lastLogin'].timetuple())
-        daysSinceLastLogin = _secsToDays(self._rolloverTime - lastLogin) - 1
+        daysSinceLastLogin = (self._rolloverTime - lastLogin) - 1
         
         if daysSinceLastLogin > self._daysUntilBoot:
             if not self._isUserSafe(uid):
