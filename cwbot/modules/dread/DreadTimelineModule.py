79bdaa64e16d6044a0155f8a19909a2c55098ce2
rlbond86 <rlbond86@96408d3d-42d1-492f-8470-de6ea9014457>
2013-09-27 00:25:23 +0000

    * DreadTimelineModule behavior modified to remember old timeline entries.
    git-svn-id: svn://svn.code.sf.net/p/cwbot/code/trunk@127 96408d3d-42d1-492f-8470-de6ea9014457

--- cwbot/modules/dread/DreadTimelineModule.py
+++ cwbot/modules/dread/DreadTimelineModule.py
@@ -1,16 +1,20 @@
 from cwbot.modules.BaseDungeonModule import BaseDungeonModule, eventDbMatch
 from cwbot.common.exceptions import FatalError
+from cwbot.common.kmailContainer import Kmail
+from cwbot.kolextra.request.UserProfileRequest import UserProfileRequest
+import pytz
 from copy import deepcopy
 import itertools
 from collections import defaultdict
 import datetime
 import time
 import re
+import threading
 from pastebin_python import PastebinPython
 from pastebin_python.pastebin_exceptions \
                      import (PastebinBadRequestException,
                              PastebinNoPastesException, PastebinFileException)
-from pastebin_python.pastebin_constants import PASTE_PUBLIC, EXPIRE_1_DAY
+from pastebin_python.pastebin_constants import PASTE_PUBLIC, EXPIRE_1_MONTH
 from pastebin_python.pastebin_formats import FORMAT_NONE
 
 
@@ -23,6 +27,7 @@ _maxLen = 33
 _format = "{:33}{:33}{:33}"
 _areas = {0: 'The Woods', 1: 'The Village', 2: 'The Castle'}
 _shortestName = 4
+_tz = pytz.timezone('America/Phoenix')
 
 class DreadTimelineModule(BaseDungeonModule):
     """ 
@@ -34,9 +39,12 @@ class DreadTimelineModule(BaseDungeonModule):
     def __init__(self, manager, identity, config):
         self._snapshots = None
         self._lastComplete = None
-        self._lastEvents = None
+        self._lastRaidlog = None
         self._apikey = None
-        self._pastebinReply = None
+        self._pastes = None
+        self._initialized = False
+        self._readyForTimeline = threading.Event()
+        self._timelineLock = threading.Lock()
         super(DreadTimelineModule, self).__init__(manager, identity, config)
 
         
@@ -44,8 +52,11 @@ class DreadTimelineModule(BaseDungeonModule):
         self._db = initData['event-db']
         self._snapshots = state['snapshots']
         self._lastComplete = state['last']
-        self._pastebinReply = state['pastebin']
+        pastes = state.get('pastes', {})
+        self._pastes = {k: v for k,v in pastes.items()
+                        if v.get('time', 0) > time.time() - 31 * 24 * 60 * 60}
         self._processLog(initData)
+        self._initialized = True
         
     
     def _configure(self, config):
@@ -54,28 +65,36 @@ class DreadTimelineModule(BaseDungeonModule):
     
     @property
     def initialState(self):
-        return {'snapshots': [], 'last': [0, 0, 0], 'pastebin': None}
+        return {'snapshots': [], 
+                'last': [0, 0, 0], 
+                'pastes': {}}
     
     
     @property
     def state(self):
         return {'snapshots': self._snapshots,
                 'last': self._lastComplete,
-                'pastebin': self._pastebinReply}
+                'pastes': self._pastes}
 
 
     def _processLog(self, raidlog):
-        events = deepcopy(raidlog['events'])
-        self._lastEvents = deepcopy(events)
-        d = self._raiseEvent("dread", "dread-overview", 
-                                       data={'style': 'list',
-                                             'keys': ['killed']})
-        killed = [data['killed'] for data in d[0].data]
-        roundedKilled = map(lambda x: (x // 50) * 50, killed)
-        if roundedKilled > self._lastComplete:
-            self._lastComplete = roundedKilled
-            self._snapshots.append(self._getNewEvents(events))
-        return True
+        with self._timelineLock:
+            events = deepcopy(raidlog['events'])
+            self._lastRaidlog = deepcopy(raidlog)
+            dvid = raidlog.get('dvid')
+            if not self._dungeonActive():
+                if dvid is not None and str(dvid) not in self._pastes:
+                    if self._initialized:
+                        self._readyForTimeline.set() 
+            d = self._raiseEvent("dread", "dread-overview", 
+                                           data={'style': 'list',
+                                                 'keys': ['killed']})
+            killed = [data['killed'] for data in d[0].data]
+            roundedKilled = map(lambda x: (x // 50) * 50, killed)
+            if roundedKilled > self._lastComplete:
+                self._lastComplete = roundedKilled
+                self._snapshots.append(self._getNewEvents(events))
+            return True
     
     
     def _getNewEvents(self, events):
@@ -121,17 +140,23 @@ class DreadTimelineModule(BaseDungeonModule):
                     newEvent = deepcopy(playerEvents[0])
                     newEvent['turns'] = eventDiff
                     newEvents.append(newEvent)
-                    print("New event: {}".format(newEvent))
         self.debugLog("Built new DB entries in {} seconds"
                       .format(time.time() - t1))
         return newEvents
     
     
     def _timelineHeader(self, events, nameShorthands):
+        r = UserProfileRequest(self.session, self.properties.userId)
+        d = self.tryRequest(r)
+        clanName = d.get('clanName')
+        kisses = self._lastRaidlog.get('dread', {}).get('kisses', "?")
+
         users = {e['userId']: e['userName'] for e in events if e['db-match']}
-        timeHeader = ("Timeline for Dreadsylvania generated {}\n\n"
-                      .format(datetime.datetime.utcnow()
-                              .strftime("%A, %B %d %Y %I:%M%p UTC")))
+        timeString = (datetime.datetime.now()
+                      .strftime("%A, %B %d %Y %I:%M%p UTC"))
+        timeHeader = ("Timeline for {}-kiss Dreadsylvania instance "
+                      "in {}\nReport generated {}\n\n"
+                      .format(kisses, clanName, timeString))
         nameHeader = ("The following name abbreviations are used in this "
                       "report:\n\n{}\n\n"
                       .format("\n".join("{} = {} (#{})"
@@ -140,6 +165,7 @@ class DreadTimelineModule(BaseDungeonModule):
                                             in nameShorthands.items())))
         topHeader = _format.format(_areas[0], _areas[1], _areas[2]) + "\n"
         return timeHeader + nameHeader + topHeader
+    
     
     # create a timeline.
     # the timeline is a list of time entries.
@@ -209,11 +235,11 @@ class DreadTimelineModule(BaseDungeonModule):
     
     # convert a timeline to a multiline string to display.
     def _timelineString(self, timeline):
-        def balanceLines(alines):
+        def balanceLines(alines, extendString):
             totalLines = map(len, alines)
             maxLines = max(totalLines)
             for lines in alines:
-                lines.extend([" |"] * (maxLines - len(lines)))
+                lines.extend([extendString] * (maxLines - len(lines)))
 
         t1 = time.time()
         areaLines = [["-+- 0% complete"], 
@@ -224,7 +250,7 @@ class DreadTimelineModule(BaseDungeonModule):
             for area in range(3):
                 for txt in t['text'][area]:
                     areaLines[area].append(" | {}".format(txt))
-            balanceLines(areaLines)
+            balanceLines(areaLines, " |")
             for area in range(3):
                 roundedKills = (t['kills'][area] // 50) * 50
                 if roundedKills > lastKills[area]:
@@ -236,7 +262,7 @@ class DreadTimelineModule(BaseDungeonModule):
                 else:
                     print("Area {}, last={}, current={}"
                           .format(area, lastKills[area], roundedKills))
-            balanceLines(areaLines)
+            balanceLines(areaLines, "-+-")
         
         # now format it correctly
         for a in range(3):
@@ -277,32 +303,89 @@ class DreadTimelineModule(BaseDungeonModule):
     
     def _processCommand(self, message, cmd, args):
         if cmd == "timeline":
-            if self._dungeonActive():
+            dvid = self._lastRaidlog.get('dvid')
+            if (self._dungeonActive() 
+                    or dvid is None
+                    or str(dvid) not in self._pastes):
                 return ("You can't get the timeline while the dungeon is "
                         "active.")
-            if not self._pastebinReply:
-                shortNames = self._getShortenedNames(self._lastEvents)
-                header = self._timelineHeader(self._lastEvents, shortNames)
-                timeline = self._eventTimeline(self._lastEvents, shortNames)
-                body = self._timelineString(timeline)
-                pbin = PastebinPython(api_dev_key=self._apikey)
-                try:
-                    result = pbin.createPaste(header + body, 
-                                          api_paste_format=FORMAT_NONE,
-                                          api_paste_private=PASTE_PUBLIC,
-                                          api_paste_expire_date=EXPIRE_1_DAY)
-                    if re.search(r'https?://', result):
-                        self._pastebinReply = result
-                    else:
-                        self.debugLog(header + body)
-                    return result
-                except (PastebinBadRequestException, 
-                        PastebinFileException, 
-                        PastebinNoPastesException) as e:
-                    return e.message
-            else:
-                return self._pastebinReply
-        return None
+            data = self._pastes[str(dvid)]
+            if data['error']:
+                return ("Error with timeline: {}".format(data['url']))
+            return "Timeline for current instance: {}".format(data['url'])
+        elif cmd == "timelines":
+            dvid = self._lastRaidlog.get('dvid')
+            timelines = self._pastes.values()
+            timelines.sort(key=lambda x: x['time'], reverse=True)
+            strings = []
+            for item in timelines:
+                urlText = item['url'] if not item['error'] else "ERROR"
+                dt_utc = datetime.datetime.fromtimestamp(item['time'],
+                                                         pytz.utc)
+                dt_az = dt_utc.astimezone(_tz)
+                timeText = dt_az.strftime("%a %d %b %y")
+                kisses = item['kisses']
+                strings.append("{} - {} [{} kisses]: {}"
+                               .format(timeText, dvid, kisses, urlText))
+            sendString = ""
+            for string in strings:
+                if len(string) + len(sendString) > 1500:
+                    break
+                sendString += string + "\n"
+            if sendString == "":
+                return "No timelines in memory."
+            self.sendKmail(Kmail(message['userId'], 
+                                 "Here are the most recent Dreadsylvania "
+                                 "instances:\n\n{}".format(sendString)))
+            return "Timelines sent."
+            
+    
+    def _heartbeat(self):
+        if self._readyForTimeline.is_set():
+            with self._timelineLock:
+                self._readyForTimeline.clear()
+                dvid = self._lastRaidlog.get('dvid')
+                if dvid is None or str(dvid) in self._pastes:
+                    return
+                self._createTimeline()
+                
+                
+    def _createTimeline(self):
+        dvid = self._lastRaidlog.get('dvid')
+        kisses = self._lastRaidlog.get('dread', {}).get('kisses', "?")
+        if dvid is None:
+            return
+        shortNames = self._getShortenedNames(self._lastRaidlog['events'])
+        header = self._timelineHeader(self._lastRaidlog['events'], shortNames)
+        timeline = self._eventTimeline(self._lastRaidlog['events'], shortNames)
+        body = self._timelineString(timeline)
+        pbin = PastebinPython(api_dev_key=self._apikey)
+        try:
+            result = pbin.createPaste(header + body, 
+                                  api_paste_format=FORMAT_NONE,
+                                  api_paste_private=PASTE_PUBLIC,
+                                  api_paste_expire_date=EXPIRE_1_MONTH)
+            resultError = re.search(r'https?://', result) is None
+            self._pastes[str(dvid)] = {'url': result,
+                                       'time': time.time(),
+                                       'dvid': dvid,
+                                       'error': resultError,
+                                       'kisses': kisses}
+            if resultError:
+                self.log("Error with timeline: {}".format(result))
+        except (PastebinBadRequestException, 
+                PastebinFileException, 
+                PastebinNoPastesException) as e:
+            self._pastes[str(dvid)] = {'url': e.message,
+                                       'time': time.time(),
+                                       'dvid': dvid,
+                                       'error': True,
+                                       'kisses': kisses}
+            self.log("Error with timeline: {}".format(e.message))
+            
+            
+    def reset(self, initData):
+        self._readyForTimeline.clear()
         
                 
     def _availableCommands(self):
