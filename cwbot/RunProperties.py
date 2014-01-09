7f63072e53c2f22d0562c9b268f441a888915122
rlbond86 <rlbond86@96408d3d-42d1-492f-8470-de6ea9014457>
2014-01-09 03:04:58 +0000

    * bumped version number
    git-svn-id: svn://svn.code.sf.net/p/cwbot/code/trunk@151 96408d3d-42d1-492f-8470-de6ea9014457

--- cwbot/RunProperties.py
+++ cwbot/RunProperties.py
@@ -33,7 +33,7 @@ class RunProperties(object):
     """
 
 
-    version = "0.12.2"
+    version = "0.12.3"
     def __init__(self, debugMode, loginFile, adminFile, 
                  originalDir=os.getcwd(), altLogin=None):
         self.debug = debugMode
