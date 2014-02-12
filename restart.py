163f1a8a0d376cd18a5aced4e2a815a30bc33199
John Kelsey (houtens) <kelsey.john.c@gmail.com>
2014-02-12 15:53:25 +0000

    wrote script to automatically restart ndy if server restarts

---
+++ restart.py
@@ -0,0 +1,28 @@
+import signal, errno
+from contextlib import contextmanager
+import fcntl
+from cwbot.main import main
+
+@contextmanager
+def timeout(seconds):
+    def timeout_handler(signum, frame):
+        pass
+
+    original_handler = signal.signal(signal.SIGALRM, timeout_handler)
+
+    try:
+        signal.alarm(seconds)
+        yield
+    finally:
+        signal.alarm(0)
+        signal.signal(signal.SIGALRM, original_handler)
+
+with timeout(1):
+    f = open("ndy.lck", "w")
+    try:
+        fcntl.flock(f.fileno(), fcntl.LOCK_EX)
+        main()
+    except IOError, e:
+        if e.errno != errno.EINTR:
+            raise e
+        print "Lock timed out"
